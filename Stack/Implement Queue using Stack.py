247f38248c35fc030ff07d1ccc72a0d72a25c8b1
Tarun <tarundb2005@gmail.com>
2026-06-27 00:15:08 +0530

    Implement queue using two stacks

---
+++ Stack/Implement Queue using Stack.py
@@ -0,0 +1,27 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+        
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+        
+    def pop(self) -> int:
+        if self.empty():
+            return "Queue is empty"
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if self.empty():
+            return "Queue is empty"
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+       
+    def empty(self) -> bool:
+        return len(self.stack1) == 0 and len(self.stack2)==0
