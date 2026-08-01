f0a0ea8917e23d5180db5be6f726187b6b808744
Tarun <tarundb2005@gmail.com>
2026-08-01 21:46:55 +0530

    Add prefix sum function for 1D array

---
+++ DSA - Revsion/Prefix Sum/1D Array Prefix.py
@@ -0,0 +1,8 @@
+def dimarray(nums):
+    prefix = [0] * len(nums)
+    prefix[0] = nums[0]
+    for i in range(1 , len(nums)):
+        prefix[i] = prefix[i-1] + nums[i]
+    return prefix
+nums = [1,2,3,4,5]
+print(dimarray(nums))
