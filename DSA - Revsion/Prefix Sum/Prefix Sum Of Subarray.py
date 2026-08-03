bb0cefbb7edfd03f65b2d7efa8d2d42ee30952d1
Tarun <tarundb2005@gmail.com>
2026-08-04 00:03:21 +0530

    Update Prefix Sum Of Subarray.py

--- DSA - Revsion/Prefix Sum/Prefix Sum Of Subarray.py
+++ DSA - Revsion/Prefix Sum/Prefix Sum Of Subarray.py
@@ -1,1 +1,15 @@
 
+def sumsub(nums, k):
+    count = 0
+    hashmap = {0: 1}
+    prefix = 0
+
+    for i in range(len(nums)):
+        prefix += nums[i]
+
+        if prefix - k in hashmap:
+            count += hashmap.get(prefix - k)
+
+        hashmap[prefix] = hashmap.get(prefix, 0) + 1
+
+    return count
