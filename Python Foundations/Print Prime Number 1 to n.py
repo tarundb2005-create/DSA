badcac92355b3e0667cf845484269bf8e5c22ec4
Tarun <tarundb2005@gmail.com>
2026-08-14 22:04:27 +0530

    Add prime number printer from 1 to n

---
+++ Python Foundations/Print Prime Number 1 to n.py
@@ -0,0 +1,12 @@
+n = int(input("Enter N: "))
+
+for num in range(2, n + 1):
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num)
