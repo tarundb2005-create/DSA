2df7b629cf31391c69f84fc1ede5ee40e1d2550d
Tarun Desu <tarundb2005@gmail.com>
2026-09-02 18:51:52 +0530

    Create Intersection of two array.py

---
+++ DSA - Revsion/Two Pointers/Merge Pointers/Intersection of two array.py
@@ -0,0 +1,18 @@
+
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        i = 0
+        j = 0
+        nums1.sort()
+        nums2.sort()
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] > nums2[j]:
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                if not result or result[-1] != nums1[i]:
+                    result.append(nums1[i])
+            i += 1
+            j += 1
+        return result
