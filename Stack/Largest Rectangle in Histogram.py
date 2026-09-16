fc0d2487d3050de0fb475e18fa546c25aa23e34b
Tarun Desu <tarundb2005@gmail.com>
2026-09-16 06:50:06 +0530

    Fix variable usage in Largest Rectangle function

--- Stack/Largest Rectangle in Histogram.py
+++ Stack/Largest Rectangle in Histogram.py
@@ -3,7 +3,7 @@ def histo(nums):
     max_area = 0
     for i in range(len(nums) + 1):
         current = nums[i] if i < len(nums) else 0
-        while stack and nums[i] < nums[stack[-1]]:
+        while stack and current < nums[stack[-1]]:
             top = stack.pop()
             right = i
             left = stack[-1] if stack else -1
@@ -11,7 +11,7 @@ def histo(nums):
             area = nums[top] * width
             max_area = max(area , max_area)
 
-       if i < len(nums):
+        if i < len(nums):
             stack.append(i)
     return max_area
 nums = [2, 1, 5, 6, 2, 3]
