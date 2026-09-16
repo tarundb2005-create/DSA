6f0d72e371f80b82b3ec42cfe9d797f704383027
Tarun Desu <tarundb2005@gmail.com>
2026-09-16 06:47:41 +0530

    Update Largest Rectangle in Histogram.py

--- Stack/Largest Rectangle in Histogram.py
+++ Stack/Largest Rectangle in Histogram.py
@@ -11,7 +11,8 @@ def histo(nums):
             area = nums[top] * width
             max_area = max(area , max_area)
 
-        stack.append(i)
+       if i < len(nums):
+            stack.append(i)
     return max_area
 nums = [2, 1, 5, 6, 2, 3]
 print(histo(nums))
