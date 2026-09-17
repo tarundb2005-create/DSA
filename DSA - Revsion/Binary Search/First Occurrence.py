1cb27fce07cbc52f796f26635adc0bb46f9d40cd
Tarun Desu <tarundb2005@gmail.com>
2026-09-17 19:08:17 +0530

    Create First Occurrence.py

---
+++ DSA - Revsion/Binary Search/First Occurrence.py
@@ -0,0 +1,17 @@
+def binary(nums , target):
+    left = 0
+    right = len(nums)-1
+    ans = -1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            ans = mid
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid -1
+    return ans
+nums = [1, 2, 2, 2, 3, 4]
+target = 2
+print(binary(nums , target))
