0172a99f7f6a7bcf296b7ac408d1b660cfd72e54
Tarun Desu <tarundb2005@gmail.com>
2026-09-19 19:01:49 +0530

    Add binary search function for upper bound

---
+++ DSA - Revsion/Binary Search/Upper Bound.py
@@ -0,0 +1,15 @@
+def binary(nums , target):
+    left = 0
+    right = len(nums)-1
+    ans = -1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] <= target:
+            ans = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return ans
+nums   = [1, 2, 2, 4, 5, 7]
+target = 2
+print(binary(nums, target))
